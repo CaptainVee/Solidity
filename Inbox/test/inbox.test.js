8b71af57611053a8f822e0e1a4f089476af1b4cb
CaptainVee <captainvee3@gmail.com>
2022-07-28 22:47:38 +0100

    deployed smart contract to rinkeby network

--- Inbox/test/inbox.test.js
+++ Inbox/test/inbox.test.js
@@ -2,22 +2,35 @@ const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
-
+const { interface, bytecode } = require('../compile')
 
 let accounts
+let inbox
 
 beforeEach(async ()=>{
 	// Get a list of all accounts
 
 	accounts = await web3.eth.getAccounts()
-		// Use one of those accounts to deploy
-	// the contract 
+
+	// Use one of those accounts to deploy the contract 
+
+	inbox = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({data: bytecode, arguments: ['Give your life to Christ']})
+		.send({ from: accounts[0], gas: '1000000' })
 })
 
 describe('Inbox', () =>{
 	it('deploys a contract', ()=>{
-		console.log(accounts)
-
+		assert.ok(inbox.options.address)
+	})
+	it('has default message', async ()=>{
+		const message = await inbox.methods.message().call()
+		assert.equal(message, 'Give your life to Christ')
+	})
+	it('can change the message', async ()=>{
+		await inbox.methods.setMessage('God is good').send({ from: accounts[0] })
+		const message = await inbox.methods.message().call()
+		assert.equal(message, 'God is good')
 	})
 })
 
